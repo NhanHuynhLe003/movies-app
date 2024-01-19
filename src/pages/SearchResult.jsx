d4d5b94e132af563a4eb78432e0d2b0980bd800c
NhanHuynh <nhan123huynh2k3@gmail.com>
2024-01-19 18:55:56 +0700

    update 1003

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -1,32 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { AppContext } from "../Context/AppContext";
 import Ipagination from "../components/Ipagination";
 import MovieCard from "../components/MovieCard";
 import MainLayout from "../layouts/MainLayout";
 import style from "../styles/searchResult.module.css";
+import SkeletonMovieCard from "../components/skeleton/skeletonMovieCard";
 export default function SearchResult() {
   //https://api.themoviedb.org/3/search/movie?api_key=2edf9f02e088272f6ff2eab6bf5fa21a&language=en-US&query=spider&page=1
   const { searchResultStorage, maxTotalPageSearch } = useContext(AppContext);
   const location = useLocation();
-
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    new Promise(() => setTimeout(() => setLoading(true), 700));
+  }, []);
   return (
     <MainLayout paramUrl={location.pathname}>
       <div id="searchResult" className={style.searchResult}>
         <section id="content" className={style.contentSection}>
           <div id="movieSearched" className={style.movieSearched}>
-            {searchResultStorage.length > 0 ? (
-              searchResultStorage.map((movie, index) => {
-                return (
-                  <MovieCard
-                    key={`${movie}-${index}`}
-                    title={movie.title}
-                    vote={movie.vote_average}
-                    posters={movie.poster_path}
-                    id={movie.id}
-                  />
-                );
-              })
+            {searchResultStorage && searchResultStorage.length > 0 ? (
+              loading ? (
+                searchResultStorage.map((movie, index) => {
+                  return (
+                    <MovieCard
+                      key={`${movie}-${index}`}
+                      title={movie.title}
+                      vote={movie.vote_average}
+                      posters={movie.poster_path}
+                      id={movie.id}
+                    />
+                  );
+                })
+              ) : (
+                <SkeletonMovieCard></SkeletonMovieCard>
+              )
             ) : (
               <h1 className={style.movieNotFound}>Movie Not Found</h1>
             )}
